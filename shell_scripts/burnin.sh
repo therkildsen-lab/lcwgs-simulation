e871ea1319bfd62fc0185be4e42290b22f1a1e61
Nicolas Lou <nicolas931010@hotmail.com>
2019-10-22 23:05:14 -0400

    Update burnin.sh

--- shell_scripts/burnin.sh
+++ shell_scripts/burnin.sh
@@ -5,13 +5,13 @@ if [ ! -d /workdir/lcwgs-simulation/sim/rep_$REP_ID ]; then
   mkdir /workdir/lcwgs-simulation/sim/rep_$REP_ID
 fi
 # Run SLiM 
-/programs/SLiM-3.3/bin/slim 
-  -d REP_ID=$REP_ID  
-  -d MUTATION_RATE=2e-7 
-  -d REC_RATE=1e-8 
-  -d CHR_LENGTH=3000 
-  -d POP_SIZE=1000 
-  -d SAMPLE_SIZE=200 
-  -d "OUT_PATH='/workdir/lcwgs-simulation/sim/'" 
-  -d "BurninFilename='Burnin.txt'" 
+/programs/SLiM-3.3/bin/slim \
+  -d REP_ID=$REP_ID  \
+  -d MUTATION_RATE=2e-7 \
+  -d REC_RATE=1e-8 \
+  -d CHR_LENGTH=3000 \
+  -d POP_SIZE=1000 \
+  -d SAMPLE_SIZE=200 \
+  -d "OUT_PATH='/workdir/lcwgs-simulation/sim/'" \
+  -d "BurninFilename='Burnin.txt'" \
   /workdir/lcwgs-simulation/slim_scripts/burnin.slim
