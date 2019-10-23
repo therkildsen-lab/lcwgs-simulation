125b7294a828bd3272ec112621afb1ae3ebe73a4
Nicolas Lou <nicolas931010@hotmail.com>
2019-10-23 12:28:28 -0400

    Create run_art.sh

---
+++ shell_scripts/run_art.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+REP_ID=$1
+OUT_DIR='/workdir/lcwgs-simulation/sim/rep_'$REP_ID'/'
+for i in {1..2}; do
+  echo '>rep_'$REP_ID > $OUT_DIR'temp_for_art.fasta'
+  tail -n 1 'derived_'$i'.fasta' >> $OUT_DIR'temp_for_art.fasta'
+  /workdir/programs/art_bin_MountRainier/art_illumina \
+  -ss HS25 \
+  -sam \
+  -i $OUT_DIR'temp_for_art.fasta' \
+  -p \
+  -na \
+  -l 150 \
+  -f 1 \
+  -m 500 \
+  -s 75 \
+  -o 'derived_'$i
+samtools view -bS -F 4 $OUT_DIR'derived_'$i'.sam' > $OUT_DIR'derived_'$i'.bam'
+rm $OUT_DIR'derived_'$i'.sam'
+rm $OUT_DIR'derived_'$i'*.aln'
+rm $OUT_DIR'derived_'$i'*.fq'
+done
