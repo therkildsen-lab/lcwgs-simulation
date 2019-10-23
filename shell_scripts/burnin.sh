fb9adfeaefa9a4bb090137a44d89ce98a0d4e51f
Nicolas Lou <nicolas931010@hotmail.com>
2019-10-22 22:58:18 -0400

    Create burnin.sh

---
+++ shell_scripts/burnin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+REP_ID=$1
+# Create output directory
+if [ ! -d /workdir/lcwgs-simulation/sim/rep_$REP_ID ]; then
+  mkdir /workdir/lcwgs-simulation/sim/rep_$REP_ID
+fi
+# Run SLiM 
+  nohup /programs/SLiM-3.3/bin/slim 
+  -d REP_ID=$REP_ID  
+  -d MUTATION_RATE=2e-7 
+  -d REC_RATE=1e-8 
+  -d CHR_LENGTH=3000 
+  -d POP_SIZE=1000 
+  -d SAMPLE_SIZE=200 
+  -d "OUT_PATH='/workdir/lcwgs-simulation/sim/'" 
+  -d "BurninFilename='Burnin.txt'" 
+  /workdir/lcwgs-simulation/slim_scripts/burnin.slim 
+  > /workdir/lcwgs-simulation/nohups/burnin_rep_$REP_ID'.nohup' &
