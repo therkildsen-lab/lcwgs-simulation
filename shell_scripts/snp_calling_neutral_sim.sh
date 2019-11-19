790d405f4ddab41bc59ff6f85c698c213db15f31
Runyang Lou <rl683@cbsunt246.biohpc.cornell.edu>
2019-11-18 22:21:18 -0500

    updated snp calling with neutral simulation script

--- shell_scripts/snp_calling_neutral_sim.sh
+++ shell_scripts/snp_calling_neutral_sim.sh
@@ -7,10 +7,10 @@ BASE_DIR='/workdir/lcwgs-simulation/neutral_sim/rep_1/'
 -b $BASE_DIR'sample_lists/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.txt' \
 -anc $BASE_DIR'slim/ancestral.fasta' \
 -out $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x' \
--dosaf 1 -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 5 -doPost 1 -doVcf 1 \
--doCounts 1 -doDepth 1 -dumpCounts 1 -doIBS 1 -makematrix 1 -doCov 1 \
--P 4 -SNP_pval 1e-6 \
--setMinDepth 2 -minInd 1 -minQ 20 \
+-dosaf 1 -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 5 \
+-doCounts 1 -doDepth 1 -dumpCounts 1 \
+-P 2 -SNP_pval 1e-6 -rmTriallelic 1e-6 \
+-setMinDepth 2 -minInd 1 -minMaf 0.0005 -minQ 20 \
 >& '/workdir/lcwgs-simulation/nohups/snp_calling_neutral_sim_1_bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.log'
 ## estimate SFS
 /workdir/programs/angsd0.931/angsd/misc/realSFS $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.saf.idx' \
