17dbc58d26c4061d4f41ce9dd2efda3c29ca8d27
Runyang Lou <rl683@cbsunt246.biohpc.cornell.edu>
2019-11-22 00:40:00 -0500

    used the newest slim build and optimized model paramters

--- shell_scripts/two_pop_sim.sh
+++ shell_scripts/two_pop_sim.sh
@@ -12,12 +12,12 @@ if [ ! -d /workdir/lcwgs-simulation/two_pop_sim/rep_$REP_ID ]; then
   mkdir slim
 fi
 # Run SLiM 
-/programs/SLiM-3.3/bin/slim \
+/workdir/programs/SLiM_build/slim \
   -d REP_ID=$REP_ID  \
   -d MUTATION_RATE=2e-7 \
-  -d SELECTION_COEFF=0.1 \
-  -d MIGRATION_RATE=0.02 \
-  -d REC_RATE=1e-8 \
+  -d SELECTION_COEFF=0.25 \
+  -d MIGRATION_RATE=0.05 \
+  -d REC_RATE=1e-7 \
   -d CHR_LENGTH=30000000 \
   -d POP_SIZE=1000 \
   -d SAMPLE_SIZE=2000 \
