21ebbdbec455ed1d4ff4a11bdf2a5b5fd3adc43f
Runyang Lou <rl683@cbsunt246.biohpc.cornell.edu>
2019-12-31 02:10:30 -0500

    added comparison with pool-seq to these

--- shell_scripts/snp_calling_neutral_sim.sh
+++ shell_scripts/snp_calling_neutral_sim.sh
@@ -7,11 +7,8 @@ BASE_DIR='/workdir/lcwgs-simulation/neutral_sim/rep_1/'
 -b $BASE_DIR'sample_lists/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.txt' \
 -anc $BASE_DIR'slim/ancestral.fasta' \
 -out $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x' \
--dosaf 1 -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 5 \
--doCounts 1 -doDepth 1 -dumpCounts 1 \
--P 4 -SNP_pval 1e-6 -rmTriallelic 1e-6 \
+-GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 5 \
+-doCounts 1 -doDepth 1 -dumpCounts 3 \
+-P 1 -SNP_pval 1e-6 -rmTriallelic 1e-6 \
 -setMinDepth 2 -minInd 1 -minMaf 0.0005 -minQ 20 \
 >& '/workdir/lcwgs-simulation/nohups/snp_calling_neutral_sim_1_bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.log'
-## estimate SFS
-/workdir/programs/angsd0.931/angsd/misc/realSFS $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.saf.idx' \
-> $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.sfs'
