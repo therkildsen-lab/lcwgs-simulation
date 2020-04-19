9ed8b217863c01fe65a6abcc008a0427d4703786
nicolas931010 <nicolas931010@hotmail.com>
2020-04-19 19:01:54 -0400

    scaled up recombination rate together with mutation rate, and added a depth filter for theta estimation (which didn't really help)

--- shell_scripts/neutral_sim.sh
+++ shell_scripts/neutral_sim.sh
@@ -14,8 +14,8 @@ fi
 # Run SLiM 
 /programs/SLiM-3.3/bin/slim \
   -d REP_ID=$REP_ID  \
-  -d MUTATION_RATE=2e-7 \
-  -d REC_RATE=1e-8 \
+  -d MUTATION_RATE=100e-8 \
+  -d REC_RATE=250e-8 \
   -d CHR_LENGTH=30000000 \
   -d POP_SIZE=1000 \
   -d SAMPLE_SIZE=2000 \
