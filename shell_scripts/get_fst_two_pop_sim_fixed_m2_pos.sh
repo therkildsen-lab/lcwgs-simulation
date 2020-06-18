1faf8ee6e3445046b74af3105ac37ab4aa4ac723
nicolas931010 <nicolas931010@hotmail.com>
2020-06-18 12:20:56 -0400

    control for maximum number of parallele jobs submitted

--- shell_scripts/get_fst_two_pop_sim_fixed_m2_pos.sh
+++ shell_scripts/get_fst_two_pop_sim_fixed_m2_pos.sh
@@ -2,24 +2,41 @@
 REP_ID=$1
 OUT_DIR_BASE=$2
 BASE_DIR=$OUT_DIR_BASE'rep_'$REP_ID'/'
+N_JOB_MAX=2
+COUNT=0
 # Generate the 2dSFS to be used as a prior for Fst estimation (and individual plots)				
 for SAMPLE_SIZE in {5,10,20,40,80,160}; do
   for COVERAGE in {0.25,0.5,1,2,4,8}; do
 		/workdir/programs/angsd0.931/angsd/misc/realSFS \
     $BASE_DIR'angsd/bam_list_p1_'$SAMPLE_SIZE'_'$COVERAGE'x.saf.idx' \
     $BASE_DIR'angsd/bam_list_p2_'$SAMPLE_SIZE'_'$COVERAGE'x.saf.idx' \
-    > $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.2dSFS' &
+    -P 8 \
+    > $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.2dSFS' & 
+    ## Submit two jobs at a time
+    COUNT=$(( COUNT + 1 ))
+    if [ $COUNT == $N_JOB_MAX ]; then
+     wait
+     COUNT=0
+    fi
   done
 done
 wait
 # Get alpha_beta
+COUNT=0
 for SAMPLE_SIZE in {5,10,20,40,80,160}; do
   for COVERAGE in {0.25,0.5,1,2,4,8}; do
 		/workdir/programs/angsd0.931/angsd/misc/realSFS fst index \
     $BASE_DIR'angsd/bam_list_p1_'$SAMPLE_SIZE'_'$COVERAGE'x.saf.idx' \
     $BASE_DIR'angsd/bam_list_p2_'$SAMPLE_SIZE'_'$COVERAGE'x.saf.idx' \
+    -P 8 \
     -sfs $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.2dSFS' \
     -fstout $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.alpha_beta' &
+    ## Submit two jobs at a time
+    COUNT=$(( COUNT + 1 ))
+    if [ $COUNT == $N_JOB_MAX ]; then
+     wait
+     COUNT=0
+    fi
   done
 done
 wait
