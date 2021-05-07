a3d91a2764463f08d60cd9e3055ffb80436023d4
nicolas931010 <nicolas931010@hotmail.com>
2021-05-07 16:10:54 -0400

    minor update

--- shell_scripts/snp_calling_spatial_pop_sim.sh
+++ shell_scripts/snp_calling_spatial_pop_sim.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 REP_ID=$1
 OUT_DIR_BASE=$2
+SUBDIR=${3:-angsd}
+GL_MODEL=${4:-1}
 BASE_DIR=$OUT_DIR_BASE'rep_'$REP_ID'/'
-N_JOB_MAX=15
+N_JOB_MAX=6
 COUNT=0
 for SAMPLE_SIZE in {5,10,20,40,80}; do
   for COVERAGE in {0.125,0.25,0.5,1,2,4}; do
@@ -10,11 +12,11 @@ for SAMPLE_SIZE in {5,10,20,40,80}; do
     /workdir/programs/angsd0.931/angsd/angsd \
     -b $BASE_DIR'sample_lists/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x.txt' \
     -anc $BASE_DIR'slim/ancestral.fasta' \
-    -out $BASE_DIR'angsd/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x' \
-    -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 5 \
+    -out $BASE_DIR$SUBDIR'/bam_list_'$SAMPLE_SIZE'_'$COVERAGE'x' \
+    -GL $GL_MODEL -doGlf 2 -doMaf 1 -doMajorMinor 5 \
     -doCounts 1 -doDepth 1 -dumpCounts 1 \
     -doIBS 2 -makematrix 1 -doCov 1 \
-    -P 2 -SNP_pval 1e-6 -rmTriallelic 1e-6 \
+    -P 4 -SNP_pval 1e-6 -rmTriallelic 1e-6 \
     -setMinDepth 2 -minInd 1 -minMaf 0.05 -minQ 20 &
     ## Submit two jobs at a time
     COUNT=$(( COUNT + 1 ))
